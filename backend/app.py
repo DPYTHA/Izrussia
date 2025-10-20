c2c5a53e2736629cb53878b6d5f8d487e94cdfb5
D-PYTHA <pythamoua@gmail.com>
2025-10-21 02:38:32 +0800

    minor changes

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,8 @@ from sqlalchemy.dialects.postgresql import JSON
 from flask_socketio import SocketIO, emit, join_room
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from config import Config  # <-- très important !
+from backend.config import Config
+
 # ---------------- CONFIG ----------------
 
 
