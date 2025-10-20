04a5ffc84ed3cccd3ab71f869d8b718bbf2a38dc
D-PYTHA <pythamoua@gmail.com>
2025-10-20 23:17:43 +0800

    minor changes

--- backend/config.py
+++ backend/config.py
@@ -2,8 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "your-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://postgresql:Pytha1991@localhost/izrussia")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://postgres:Pytha1991@localhost:5432/izrussia")
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
